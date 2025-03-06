8f0dec7b90e5d43a794ac3d57f8e77c5ee8bd488
jiqiemuyi <jiqiemuyi@gmail.com>
2025-03-06 16:52:48 +0800

    wip:新增 latest-version

--- bin/cli.js
+++ bin/cli.js
@@ -1,37 +1,46 @@
 #!/usr/bin/env node
 const { program } = require("commander");
-const updateNotifier = require("update-notifier");
 const pkg = require("../package.json");
-// 检查更新
-updateNotifier({ pkg }).notify();
+const latestVersion = require("latest-version");
 
-program
-  .name(Object.keys(pkg.bin)[0])
-  .description("A CLI tool for managing React projects")
-  .version(pkg.version)
-  .showHelpAfterError(); // 启用错误后显示帮助信息
+(async () => {
+  const currentVersion = pkg.version;
+  const latest = await latestVersion(pkg.name);
 
-// 定义 create 子命令
-program
-  .command("create <project-name>")
-  .description("Create a new project")
-  .option(
-    "-t, --template <template-name>",
-    "Specify a template (default: default)",
-    "default"
-  )
-  .option("--typescript", "Use TypeScript for the project")
-  .action((projectName, options) => {
-    console.log("log", projectName, options);
-    require("../src/commands/create")(projectName, options);
-  });
+  if (latest !== currentVersion) {
+    console.log(`Update available: ${latest} (current: ${currentVersion})`);
+  } else {
+    console.log("No updates available.");
+  }
 
-// 定义 init 子命令
-program
-  .command("init")
-  .description("Initialize a configuration file")
-  .action(() => {
-    console.log("Initializing configuration file...");
-  });
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .description("A CLI tool for managing React projects")
+    .version(pkg.version)
+    .showHelpAfterError(); // 启用错误后显示帮助信息
 
-program.parse(process.argv);
+  // 定义 create 子命令
+  program
+    .command("create <project-name>")
+    .description("Create a new project")
+    .option(
+      "-t, --template <template-name>",
+      "Specify a template (default: default)",
+      "default"
+    )
+    .option("--typescript", "Use TypeScript for the project")
+    .action((projectName, options) => {
+      console.log("log", projectName, options);
+      require("../src/commands/create")(projectName, options);
+    });
+
+  // 定义 init 子命令
+  program
+    .command("init")
+    .description("Initialize a configuration file")
+    .action(() => {
+      console.log("Initializing configuration file...");
+    });
+
+  program.parse(process.argv);
+})();
